a80bf7bed6a7d946df5d1e800e8bb489411c84da
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-04-05 15:26:33 -0400

    inject people into header

--- src/js/modules/data.js
+++ src/js/modules/data.js
@@ -87,8 +87,6 @@ module.exports =  {
             return b.left - a.left;
         });
 
-        this.calculateStats();
-
         this.injectHTML();
     },
 
@@ -102,113 +100,12 @@ module.exports =  {
         return Math.round(diff / 86400000);
     },
 
-    calculateStats: function() {
-        window.stats = [];
-
-        window.stats.push(this.getWorstPosition());
-        window.stats.push(this.getFiredViaTweet());
-        window.stats.push(this.getShortestStint());
-        window.stats.push(this.getProflicDay());
-        console.log(stats);
-    },
-
     convertGridToUrl: function(gridUrl) {
-        // https://media.gutools.co.uk/images/e6519c921186c8511a688036b061bd749090d740?crop=1192_0_1355_1355
-        // https://media.guim.co.uk/e6519c921186c8511a688036b061bd749090d740/1192_0_1355_1355/140.jpg
-
         return gridUrl.replace('gutools.co.uk', 'guim.co.uk').replace('images/', '').replace('?crop=', '/') + '/140.jpg';
-    },
-
-    getWorstPosition: function() {
-        var position;
-        var highestCount = 0;
-
-        for (var i in positions) {
-            if (positions[i].count > highestCount) {
-                position = positions[i];
-                highestCount = positions[i].count;
-            }
-        }
-
-        return {
-            number: highestCount,
-            fact: 'former ' + position.position + 's',
-            detail: 'Making it the most tenuous job under Trump'
-        }
-    },
-
-    getFiredViaTweet: function() {
-        var count = 0;
-
-        for (var i in data) {
-            if (data[i].viaTweet == 'TRUE') {
-                count++;
-            }
-        }
-
-        return {
-            number: count,
-            fact: 'Learnt of their fate via Twitter',
-            detail: 'That\'s ' + count + ' more than any other president'
-        }
-    },
-
-    getShortestStint: function() {
-        var person;
-        var shortestActive = 500;
-
-        for (var i in data) {
-            if (shortestActive > data[i].daysActive) {
-                person = data[i];
-                shortestActive = data[i].daysActive;
-            }
-        }
-
-        return {
-            number: shortestActive,
-            fact: 'days as the ' + person.position,
-            detail: person.name + ' has served the shortest stint under Trump\'s tenure'
-        }
-    },
-
-    getProflicDay: function() {
-        var days = [];
-
-        for (var i in data) {
-            days.push(data[i].left);
-        }
-
-        var counts = {};
-        var highestCount = 0;
-        var proflicDay;
-
-        for (var i in days) {
-            var day = days[i];
-
-            if (counts[day] === undefined) {
-                counts[day] = 1;
-            } else {
-                counts[day] = counts[day] + 1;
-            }
-
-            if (counts[day] > highestCount) {
-                highestCount = counts[day];
-                proflicDay = days[i];
-            }
-        }
-
-        var months = ['January', 'February', 'March' , 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-        return {
-            number: highestCount,
-            fact: 'departures on ' + proflicDay.getDate() + ' ' + months[proflicDay.getMonth()],
-            detail: 'Making that day Trump\'s most proflic yet'
-        }
     },
 
     injectHTML: function() {
         this.addCards();
-        this.addStats();
         this.showContent();
     },
 
@@ -216,7 +113,11 @@ module.exports =  {
         var personHtml = require('../templates/person.html');
         var personTemplate = handlebars.compile(personHtml);
 
+        var headerHtml = require('../templates/header.html');
+        var headerTemplate = handlebars.compile(headerHtml);
+
         for (var i in data) {
+            $('.js-header').append(headerTemplate(data[i]));
             if (i == 0) {
                 $('.js-latest').append(personTemplate(data[i]));
             } else {
@@ -225,16 +126,12 @@ module.exports =  {
         }
     },
 
-    addStats: function() {
-        var statHtml = require('../templates/stat.html');
-        var statTemplate = handlebars.compile(statHtml);
-
-        for (var i in stats) {
-            $('.js-numbers').append(statTemplate(stats[i]));
-        }
+    showContent: function() {
+        this.triggerHeader();
+        $('.js-fire').addClass('is-visible');
     },
 
-    showContent: function() {
-        $('.js-fire').addClass('is-visible');
+    triggerHeader: function() {
+        $('.fire-header__scroll').scrollLeft(40000);
     }
 };
