b993d97c6d8280a308307bd5fde4eb37056a2a16
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-04-09 14:37:11 -0400

    add cutouts and years

--- src/js/modules/data.js
+++ src/js/modules/data.js
@@ -55,7 +55,7 @@ module.exports =  {
             date = new Date(date);
             months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-            return date.getDate() + ' ' + months[date.getMonth()];
+            return date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
         });
     },
 
